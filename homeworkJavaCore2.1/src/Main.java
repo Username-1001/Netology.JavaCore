09deee65e21edfb54368b825131b82abeab61d32
Ravermeister <bgygblbketbl@mail.ru>
2022-11-24 09:45:02 +0300

    hw2 updated

--- homeworkJavaCore2.1/src/Main.java
+++ homeworkJavaCore2.1/src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> result = new ArrayList<>();
+
+        for (Integer comp : intList) {
+            if (comp > 0 && comp % 2 == 0) {
+                result.add(comp);
+            }
+        }
+
+        Collections.sort(result, Comparator.naturalOrder());
+
+        System.out.println(result);
+    }
 }
--- homeworkJavaCore2.2/src/Main.java
+++ homeworkJavaCore2.2/src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        //п. 1
+        long teenCount = persons.stream().filter(person -> person.getAge() < 18).count();
+        // п.2
+        List<String> recruitNames = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() > 18 && person.getAge() < 27)
+                .map(person -> person.getName())
+                .collect(Collectors.toList());
+        //п.3
+        List<Person> employables = persons.stream()
+                .filter(person -> person.getAge() > 18 && person.getEducation() == Education.HIGHER &&
+                        ((person.getSex() == Sex.MAN && person.getAge() < 65) ||
+                        (person.getSex() == Sex.WOMAN && person.getAge() < 60)))
+                .sorted(Comparator.comparing(Person::getName))
+                .collect(Collectors.toList());
+
+    }
 }
